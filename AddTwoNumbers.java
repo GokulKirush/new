44bdb2bed8cf41077c84e368caf10097fa1afae0
GokulKirush <gokulkirushgk@gmail.com>
2025-10-27 21:01:02 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -4,8 +4,9 @@ public class AddTwoNumbers {
     public static void main(String[] args) {
        int num1 = 5;
         int num2 = 10;
-        int sum = add(num1, num2);
+        int sum = num1+ num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
+
 
