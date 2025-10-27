87a72279982ea3256f2b5b2fd5c57527e6a5e30f
GokulKirush <gokulkirushgk@gmail.com>
2025-10-27 20:51:22 +0530

    Refactor AddTwoNumbers to use fixed inputs

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,19 +1,11 @@
-import java.util.Scanner;
+
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
-
-        int sum = num1 + num2;
-
-        System.out.println("The sum is: " + sum);
-
-        scanner.close();
+       int num1 = 5;
+        int num2 = 10;
+        int sum = add(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
+
